62c42c60567e70b8c72f563fadebce733fafaf77
15968105952 <1824902907@qq.com>
2021-02-05 10:18:24 +0800

    第一次提交

---
+++ mylibrary/src/main/java/com/example/mylibrary/tabview/GradientColorTextView.java
@@ -0,0 +1,42 @@
+package com.example.mylibrary.tabview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Copyright (C), 2018-2018
+ * FileName:
+ * Author:       mz
+ * Date:         2019/4/25 16:17
+ * Description:  ${DESCRIPTION}
+ */
+public class GradientColorTextView extends TextView {
+
+    private LinearGradient mLinearGradient;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private Rect mTextBound = new Rect();
+
+    public GradientColorTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mViewWidth = getMeasuredWidth();
+        mPaint = getPaint();
+        String mTipText = getText().toString();
+        mPaint.getTextBounds(mTipText, 0, mTipText.length(), mTextBound);
+        mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
+                new int[]{0xFFDC63FF, 0xFFFF6F6F},
+                null, Shader.TileMode.REPEAT);
+        mPaint.setShader(mLinearGradient);
+        canvas.drawText(mTipText, getMeasuredWidth() / 2 - mTextBound.width() / 2, mTextBound.height(), mPaint);
+    }
+}
